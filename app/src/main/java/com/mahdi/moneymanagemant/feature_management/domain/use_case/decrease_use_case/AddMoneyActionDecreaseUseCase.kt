0e0bf60fa20ee1cb22189b4d1d47fd9e686df617
bob <medise08@gmail.com>
2022-05-30 01:58:28 +0430

    error-deleted

--- app/src/main/java/com/mahdi/moneymanagemant/feature_management/domain/use_case/decrease_use_case/AddMoneyActionDecreaseUseCase.kt
+++ app/src/main/java/com/mahdi/moneymanagemant/feature_management/domain/use_case/decrease_use_case/AddMoneyActionDecreaseUseCase.kt
@@ -1,6 +1,5 @@
 package com.mahdi.moneymanagemant.feature_management.domain.use_case.decrease_use_case
 
-import com.mahdi.moneymanagemant.feature_management.domain.model.money_increase_model.InvalidException
 import com.mahdi.moneymanagemant.feature_management.domain.model.money_decrease_model.InvalidExceptionDecrease
 import com.mahdi.moneymanagemant.feature_management.domain.model.money_decrease_model.MoneyManagementDecrease
 import com.mahdi.moneymanagemant.feature_management.domain.repository.decrease_repository.MoneyManagementDecreaseRepository
@@ -12,22 +11,16 @@ class AddMoneyActionDecreaseUseCase(
      @Throws(InvalidExceptionDecrease::class)
      suspend operator fun invoke(moneyManagementDecrease: MoneyManagementDecrease) {
           if (moneyManagementDecrease.nameDecrease.isBlank()) {
-               throw InvalidException("Set Title")
+               throw InvalidExceptionDecrease("Set Title")
           }
           if (moneyManagementDecrease.nameDecrease.length > 15){
-               throw InvalidException("long Title")
+               throw InvalidExceptionDecrease("long Title")
           }
           if (moneyManagementDecrease.contentDecrease.isBlank()) {
-               throw InvalidException("Set Content")
-          }
-          if (moneyManagementDecrease.cardNumberDecrease.isBlank()) {
-               throw InvalidException("Set Card Number")
-          }
-          if (moneyManagementDecrease.cardNumberDecrease.length < 16 || moneyManagementDecrease.cardNumberDecrease.length >16){
-               throw InvalidException("CardNumber Should be 16 character")
+               throw InvalidExceptionDecrease("Set Content")
           }
           if (moneyManagementDecrease.priceDecrease.isBlank()) {
-               throw InvalidException("Set Price")
+               throw InvalidExceptionDecrease("Set Price")
           }
           repository.insertMoneyAction(moneyManagementDecrease)
      }
